dc68398741bdaacfe702630af025e4b71bee9821
ionloi <flying9199@gmail.com>
2022-05-27 20:08:24 +0900

    Second Refactoring

---
+++ 2day/Permutation/Permutation.mjs
@@ -0,0 +1,19 @@
+//s = 탐색 시작할 원소의 index
+//r = 뽑을 원소 개수
+
+function permutation(arr, s, r){
+    if(s == r){
+        count++;
+        console.log(arr.join(" "));
+        return;
+    }
+
+    for(let i = s; i < arr.length; i++){
+        [arr[s], arr[i]] = [arr[i], arr[s]]; //swap
+        permutation(arr, s+1, r);
+        [arr[s], arr[i]] = [arr[i], arr[s]]; //원상복귀
+    }
+}
+
+
+export {permutation};
