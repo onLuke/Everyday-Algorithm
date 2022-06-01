d75c5875e48f9dbc859cabf32d80e8e31ac2942e
ionloi <flying9199@gmail.com>
2022-06-01 18:01:36 +0900

    Rename

--- 0001day/Combination/Combination.mjs
+++ 0001day/Combination/Combination.mjs
@@ -9,7 +9,6 @@
     }
 
       for(let i = originIdx; arr.length-i >= selectNumber - startIndex; i++){
-
         // 조합은 기준 원소 이후의 원소들만 골라야함.
         // length가 r(고를 갯수) - s(초기값) 보다 크거나 같다는 것은, 
         // 기준 원소(s) 이후에 원소가 고를 갯수(r)만큼 남아 있을 때 까지만 수행한다는 뜻이다.
